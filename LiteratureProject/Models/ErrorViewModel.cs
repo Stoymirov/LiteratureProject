4c24b6f9a576bb3825afa9324dd77daba2e7e4b2
Stoymirov <dimitarstoymirov0@gmail.com>
2024-10-20 20:40:32 +0300

    Add project files.

---
+++ LiteratureProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LiteratureProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
