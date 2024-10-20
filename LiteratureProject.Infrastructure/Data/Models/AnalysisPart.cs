e14f722678c8b99fd71ec4affc887cdc62a7811c
Stoymirov <dimitarstoymirov0@gmail.com>
2024-10-20 21:33:46 +0300

    Added LiteratureWork, AnalysisPart, Author

---
+++ LiteratureProject.Infrastructure/Data/Models/AnalysisPart.cs
@@ -0,0 +1,18 @@
+﻿using LiteratureProject.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteratureProject.Infrastructure.Data.Models
+{
+    public class AnalysisPart
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int LiteratureWorkId { get; set; }
+        public LiteratureWork LiteratureWork { get; set; } = null!;
+    }
+}
--- LiteratureProject.Infrastructure/Data/Models/LiteratureWork.cs
+++ LiteratureProject.Infrastructure/Data/Models/LiteratureWork.cs
@@ -1,8 +1,18 @@
-﻿namespace LiteratureProject.Data.Models
+﻿using LiteratureProject.Infrastructure.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LiteratureProject.Data.Models
 {
     public class LiteratureWork
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int AuthorId { get; set; }
+
+        [ForeignKey(nameof(AuthorId))]
+        public Author Author { get; set; }
+
+        public IEnumerable<ApplicationUser> CreatorsOfAnalysis { get; set; } = new List<ApplicationUser>();
+        public IEnumerable<AnalysisPart> AnalysisParts { get; set; } = new List<AnalysisPart>();
     }
 }
