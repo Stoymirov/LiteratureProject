8f535b96a0f13dd9d46aef1b0ec66e2de4a3a029
Stoymirov <dimitarstoymirov0@gmail.com>
2024-11-16 22:27:24 +0200

    asigningPropery

--- LiteratureProject/Areas/Admin/Models/AssignRoleViewModel.cs
+++ LiteratureProject/Areas/Admin/Models/AssignRoleViewModel.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LiteratureProject.Areas.Admin.Models
-{
-    public class AssignRoleViewModel
     {
-        public List<SelectListItem> Users { get; set; } = new List<SelectListItem>();
-        public List<SelectListItem> Roles { get; set; } = new List<SelectListItem>();
+        public class AssignRoleViewModel
+        {
+            public List<SelectListItem> Users { get; set; } = new List<SelectListItem>();
+            public List<SelectListItem> Roles { get; set; } = new List<SelectListItem>();
 
-        public string SelectedUserId { get; set; } = string.Empty;
-        public string SelectedRole { get; set; } = string.Empty;
+            public string SelectedUserId { get; set; } = string.Empty;
+            public string SelectedRole { get; set; } = string.Empty;
+        }
     }
-}
